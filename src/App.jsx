adf059d3d925a1e728f514fccdeaf843f166e0d8
maxsaunders8794@gmail.com <maxsaunders8794@gmail.com>
2022-10-11 00:16:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+const Album = () =>
+<div className='album'>
+  <img className='album-artwork' src='https://source.unsplash.com/random/300x300?v=1' />
+  <div className='album-details'>
+    <h2>Album Title</h2>
+    <p className='album-artist'>Artist Name</p>
+    <p className='album-desc'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et.</p>
+    <p className='album-desc'>Lorem ipsum dolor sit amet</p>
+  </div>
+</div>
+
+function App() {
+
+  return (
+    <div className="albums">
+      <Album />
+      <Album />
+      <Album />
+      <Album />
+    </div>
+  )
+}
+
+export default App
